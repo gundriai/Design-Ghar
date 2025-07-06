39819b152ba681f01d46eb7f59d46cd38a0890c0
sudhirVoid <bhandarisudhir4274@gmail.com>
2025-07-06 15:49:07 +0530

    integrated user role

--- Design-ghar-backend/src/main.ts
+++ Design-ghar-backend/src/main.ts
@@ -6,6 +6,8 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({ origin: true });
+
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
