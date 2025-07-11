f737981846ff6241a75bec52db31fbb07685c5e0
sudhirVoid <bhandarisudhir4274@gmail.com>
2025-07-11 22:30:43 +0530

    service part integrated to front end

--- Design-Ghar-UI/src/pages/Home.tsx
+++ Design-Ghar-UI/src/pages/Home.tsx
@@ -7,9 +7,11 @@ import ServiceCard from '@/components/home/ServiceCard';
 import ProductCard from '@/components/home/ProductCard';
 import { Skeleton } from '@/components/ui/skeleton';
 import { useProduct } from '@/context/ProductContext';
+import { categoryData } from '@/context/CategoryContext';
 
 export default function Home() {
-  const { services, banners, isLoading } = useData();
+  const {banners, isLoading } = useData();
+  const {categories} = categoryData();
   const {products} = useProduct();
   
   // Get featured products
@@ -50,8 +52,8 @@ export default function Home() {
             </div>
           ) : (
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-              {services.map((service) => (
-                <ServiceCard key={service.id} {...service} />
+              {categories.map((category) => (
+                <ServiceCard key={category.id} {...category} />
               ))}
             </div>
           )}
